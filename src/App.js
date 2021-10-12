4706b5aef9c4796a9f05d096d6e7ca3a79512d61
BC-macair003 <bc-macair003@BC-macair003s-MacBook-Air.local>
2021-10-12 14:36:06 +0300

    created components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
 import './App.css';
-const Box= (props)=>{
-  return <div className="box">
-    <div>
-<h2>{props.name}</h2>
-<p>Title: {props.title}</p>
-<p>Age: {props.age}</p>  </div>
 
-</div>
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 
-};
+
 function App() {
   return (
-    <div >
-    <header>
-<h1>Logo</h1>
-</header>
-<main>
-  <Box name="MAria" title="CEO" age="29"/>
-  <Box name="MArta" title="Designer" age="49"/>
-  <Box name="MAntyla" title="MD" age="89"/>
- 
-
-</main>
-<footer>
-<p><small>Copyrights</small></p>
-</footer>
-
-
-
-
-      
-  
-        
+    <div>
+    <Header/>
+    <Main/>
+    <Footer/>
     </div>
+    
   );
-}
+};
+
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import Box from "./Box";
+const Main=()=>{
+    return(
+<main>
+  <Box name="MAria" title="CEO" age="29"/>
+  <Box name="MArta" title="Designer" age="49"/>
+  <Box name="MAntyla" title="MD" age="89"/>
+ 
+
+</main>
+    );
+};
+
+export default Main;
